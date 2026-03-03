b3487177f4551b05d0da5524b79eb48d0f33ef55
Nathan de Moura <nathandemoura7@gmail.com>
2026-03-03 13:50:11 -0300

    adicao de banco de dados

---
+++ Estoque.Repositorio/produtoRepositorioSql.cs
@@ -0,0 +1,46 @@
+using Estoque.Dominio.Interfaces;
+using Estoque.Dominio.Models;
+using Estoque.Repositorio.Data;
+using Microsoft.EntityFrameworkCore; // Adicione este se não tiver
+
+namespace Estoque.Repositorio;
+
+public class ProdutoRepositorioSql : IProdutoRepositorio
+{
+    private readonly AppDbContext _context;
+
+    public ProdutoRepositorioSql(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Produto> ObterTodos() 
+    {
+        return _context.Produtos.ToList(); 
+    }
+
+    public Produto ObterPorId(int id) => _context.Produtos.Find(id);
+    
+    public void Adicionar(Produto produto) 
+    { 
+        _context.Produtos.Add(produto); 
+        _context.SaveChanges(); 
+    }
+    
+public void Atualizar(Produto produto)
+{
+    _context.Produtos.Update(produto);
+    
+    _context.SaveChanges();
+}
+public void Remover(int id)
+{
+    var produto = _context.Produtos.Find(id);
+    
+    if (produto != null)
+    {
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+    }
+}
+}
--- apiEstoque/Program.cs
+++ apiEstoque/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Estoque.Repositorio.Data;
+using Estoque.Repositorio;
+using Estoque.Dominio.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddScoped<Estoque.Dominio.Interfaces.IProdutoRepositorio, Estoque.Repositorio.ProdutoRepositorioJson>();
+builder.Services.AddScoped<IProdutoRepositorio, ProdutoRepositorioSql>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=../estoque.db"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,7 +24,4 @@ app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
